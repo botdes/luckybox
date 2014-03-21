6b4a1e36f22f0ed1ee9ec1cd6e3ec1d9170fe19f
Denis Bykov <denisbykov@yandex-team.ru>
2014-03-22 00:14:44 +0400

    test web application with embedded jetty & spring mvc (based on https://github.com/bkielczewski/example-spring-mvc-jetty)

---
+++ luckybox-web-testapp/src/main/java/ru/cleancode/luckybox/webtestapp/config/AppConfig.java
@@ -0,0 +1,10 @@
+package ru.cleancode.luckybox.webtestapp.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages="ru.cleancode.luckybox.webtestapp")
+public class AppConfig {
+
+}
---
+++ luckybox-web-testapp/src/main/java/ru/cleancode/luckybox/webtestapp/controller/IndexController.java
@@ -0,0 +1,17 @@
+package ru.cleancode.luckybox.webtestapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SuppressWarnings("UnusedDeclaration")
+public class IndexController {
+
+    @RequestMapping(value="/", method = RequestMethod.GET)
+    @ResponseBody
+    public String showIndex() {
+        return "Hello, world!";
+    }
+}
